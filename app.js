528427acb90cbc268a2788ba3144aeff4074a665
Ogar Simon <ogarsimon0000@gmail.com>
2023-01-05 19:57:58 +0100

    update

--- app.js
+++ app.js
@@ -6,11 +6,28 @@ const jobsRouter = require("./routes/jobs");
 const authRouter = require("./routes/auth");
 const auth = require("./middleware/authentication");
 
+const helment = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 100,
+    max: 100,
+  })
+);
+
 app.use(express.json());
+
+app.use(helment());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 const connectDB = require("./db/connect");
